84ab65d7c5be9734270da0097d0508a1d24d174a
minjijeong98 <minji.jeong.ds@gmail.com>
2024-09-20 14:50:13 +0000

    feat: 최고 성능 모델만 유지하도록 모델 저장 방식 변경
    - 기존: 누적으로 좋은 모델들 모두 저장
    - 변경: 지금까지 중 가장 성능이 좋은 모델만 저장

--- torch_code/utils/utils.py
+++ torch_code/utils/utils.py
@@ -2,6 +2,7 @@ import yaml
 from box import Box
 import torch
 import os
+import glob
 
 def load_config(config_file):
     # Load Config.yaml
@@ -17,7 +18,13 @@ def ckpt_save(model, model_name, optimizer, epoch, pearson, best_pearson):
     if not os.path.exists(model_directory):
         os.makedirs(model_directory)
     
-    save_path = f"{model_name.split('/')[1]}_best_model_Pearson_{pearson}_epoch_{epoch}.pt"
+    # delete previous best-model
+    previous_best = glob.glob(os.path.join(model_directory, f"{model_name.split('/')[1]}_best_model_Pearson_*_epoch_*.pt"))
+    for file in previous_best:
+        os.remove(file)
+
+    # save new best-model
+    save_path = f"{model_name.split('/')[1]}_best_model_Pearson_{pearson:.4f}_epoch_{epoch}.pt"
     torch.save(model, os.path.join(model_directory,save_path))
-    
-    print(f"Model Saved at Pearson {best_pearson} to {pearson}")
+
+    print(f"Model improved from Pearson {best_pearson:.4f} to {pearson:.4f} and saved.")
