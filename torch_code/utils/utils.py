c366f5b31e29102db9c65121e9b21562ec859eea
minjijeong98 <minji.jeong.ds@gmail.com>
2024-09-22 12:02:50 +0000

    feat: validation criterion 변경
    - 기존: pearson coefficient
    - 변경: train에서 사용되는 것과 동일한 loss

--- torch_code/utils/utils.py
+++ torch_code/utils/utils.py
@@ -12,19 +12,19 @@ def load_config(config_file):
 
     return config
 
-def ckpt_save(model, model_name, optimizer, epoch, pearson, best_pearson):
+def ckpt_save(model, model_name, optimizer, epoch, loss, minimum_loss):
 
     model_directory = "./saved_model"
     if not os.path.exists(model_directory):
         os.makedirs(model_directory)
     
     # delete previous best-model
-    previous_best = glob.glob(os.path.join(model_directory, f"{model_name.split('/')[1]}_best_model_Pearson_*_epoch_*.pt"))
+    previous_best = glob.glob(os.path.join(model_directory, f"{model_name.split('/')[1]}_validation_loss_*_epoch_*.pt"))
     for file in previous_best:
         os.remove(file)
 
     # save new best-model
-    save_path = f"{model_name.split('/')[1]}_best_model_Pearson_{pearson:.4f}_epoch_{epoch}.pt"
+    save_path = f"{model_name.split('/')[1]}_validation_loss_{loss:.4f}_epoch_{epoch}.pt"
     torch.save(model, os.path.join(model_directory,save_path))
 
-    print(f"Model improved from Pearson {best_pearson:.4f} to {pearson:.4f} and saved.")
+    print(f"Model improved from validation loss {minimum_loss:.4f} to {loss:.4f} and saved.")
