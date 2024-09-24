b62cc652bc3aff28273a5346cae4952a54f46d57
오수현 <elena0315@naver.com>
2024-09-24 11:44:57 +0900

    fix: label normalization

--- huggingface_code/train.py
+++ huggingface_code/train.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
 
     config = configurer(arg.config)
 
-    train_dataset = preprocess(task="train", data_path=config.train_path, model_name=config.model)
-    valid_dataset = preprocess(task="valid", data_path=config.valid_path, model_name=config.model)
+    train_dataset = preprocess(task="train", data_path=config.train_path, model_name=config.model, scale=config.scale)
+    valid_dataset = preprocess(task="valid", data_path=config.valid_path, model_name=config.model, scale=config.scale)
 
     model = Model(model_name=config.model,
                   output_dir=config.output_dir,
